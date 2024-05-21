179e3ff0bfc4b289e249a864f01d53678687afba
Pranav Singh <1329318+RuinedSubmartingale@users.noreply.github.com>
2024-05-21 02:07:41 +0000

    added Uploaded time element and mirrored the image vertically below

--- 01_01b/script.js
+++ 01_01b/script.js
@@ -9,30 +9,42 @@ import data from "./data.js";
 
 const mainContent = document.querySelector(".main-content");
 
+function createImage(imgData, additionalClass = "") {
+  return `
+    <img
+      class="${additionalClass}"
+      srcset="
+        ${imgData.urls.full} ${imgData.width}w,
+        ${imgData.urls.regular} 1080w,
+        ${imgData.urls.small} 400w
+      "
+      sizes="(max-width: 450px) 400px, (max-width: 800) 1080px"
+      src="${imgData.urls.regular}"
+      width="${imgData.width}"
+      height="${imgData.height}"
+      alt="${imgData.description}"
+      loading="lazy"
+    />
+  `;
+}
+
 const Card = (data) => {
   const imgData = data[0];
 
   const markup = `
     <figure class="image">
-      <img
-        srcset="
-          ${imgData.urls.full} ${imgData.width}w,
-          ${imgData.urls.regular} 1080w,
-          ${imgData.urls.small} 400w
-        "
-        sizes="(max-width: 450px) 400px, (max-width: 800) 1080px"
-        src="${imgData.urls.regular}"
-        width="${imgData.width}"
-        height="${imgData.height}"
-        alt="${imgData.description}"
-        loading="lazy"
-      />
+      ${createImage(imgData)}
+      ${createImage(imgData, "inverted-image")}
       <figcaption class="image__caption">
         <h3 class="image__title">${imgData.description}</h3>
         <div class="image__meta">
           <p>
             Photo by
             <span class="image__photog">${imgData.user.name}</span>.
+          </p>
+          <p>
+            Uploaded on
+            <time class="image__date" datetime="">${imgData.created_at}</time>
           </p>
           <p>
             <a href="${imgData.links.self}" class="image__link">
