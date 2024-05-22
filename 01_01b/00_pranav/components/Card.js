5a9f16d8580f7aa642e73d4ffe5db438310db9b1
Pranav Singh <1329318+RuinedSubmartingale@users.noreply.github.com>
2024-05-22 03:42:21 +0000

    destructured imgData args in Card.js

--- 01_01b/00_pranav/components/Card.js
+++ 01_01b/00_pranav/components/Card.js
@@ -1,18 +1,23 @@
-const buildImage = (imgData) => {
-  let srcset = `${imgData.urls.full} ${imgData.width}w`;
-  if (imgData.urls.regular) {
-    srcset += `, ${imgData.urls.regular} 1080w`;
-  } else if (imgData.urls.small) {
-    srcset += `, ${imgData.urls.small} 400w`;
+const buildImage = ({
+  width,
+  height,
+  description,
+  urls: { full, regular, small },
+}) => {
+  let srcset = `${full} ${width}w`;
+  if (regular) {
+    srcset += `, ${regular} 1080w`;
+  } else if (small) {
+    srcset += `, ${small} 400w`;
   }
   return `
     <img
       srcset="${srcset}"
       sizes="(max-width: 450px) 400px, (max-width: 800) 1080px"
-      src="${imgData.urls.regular}"
-      width="${imgData.width}"
-      height="${imgData.height}"
-      alt="${imgData.description}"
+      src="${regular}"
+      width="${width}"
+      height="${height}"
+      alt="${description}"
       loading="lazy"
     />
   `;
@@ -28,24 +33,31 @@ const getDate = (imgData) => {
 };
 
 const Card = (imgData) => {
+  const {
+    description,
+    user: { name },
+    created_at: createdDate,
+    links: { self },
+  } = imgData;
+
   const markup = `
     <figure class="image">
       ${buildImage(imgData)}
       
       <figcaption class="image__caption">
-        <h3 class="image__title">${imgData.description}</h3>
+        <h3 class="image__title">${description}</h3>
         <div class="image__meta">
           <p>
             Photo by
-            <span class="image__photog">${imgData.user.name}</span>.
+            <span class="image__photog">${name}</span>.
           </p>
           <p>
-            Uploaded on <time class="image__date" datetime="${
-              imgData.created_at
-            }">${getDate(imgData)}</time>
+            Uploaded on <time class="image__date" datetime="${createdDate}">${getDate(
+    imgData
+  )}</time>
           </p>
           <p>
-            <a href="${imgData.links.self}" class="image__link">
+            <a href="${self}" class="image__link">
               View it on Unsplash.
             </a>
           </p>
